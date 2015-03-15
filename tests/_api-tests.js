dbf60785a76fb62f74d3facef58b5148acefd4ff
Jack Davis <mr.jackdavis@gmail.com>
2015-03-15 18:47:53 +1100

    Scaffold unit tests

--- tests/_api-tests.js
+++ tests/_api-tests.js
@@ -1,3 +1,23 @@
-describe('API endpoint for Scene',function(){
-    console.log('hello world');
+describe('API endpoint',function(){
+	describe('/scene',function(){
+		describe('POST',function(){
+			it('should return all scenes in JSON',function(){
+				
+			});
+			it('should respond with code 200 upon success',function(){
+				
+			});
+		});
+		describe('POST',function(){
+			it('should create a new scene',function(){
+
+			});
+			it('should respond with code 201 upon success',function(){
+
+			});
+			it('should return a Location header with a link to the newly-created resource',function(){
+
+			});
+		});
+	});
 });
