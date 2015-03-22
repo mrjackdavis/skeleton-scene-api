f9b9fd5660e042e80a73f5a9270d6cd516ebe973
Jack Davis <mr.jackdavis@gmail.com>
2015-03-22 13:47:28 +1100

    Further flesh out GET and POST tests

--- controllers/scene-api.js
+++ controllers/scene-api.js
@@ -1,7 +1,9 @@
 var router = require('express').Router();
 
 router.get('/',function (req,res) {
-	res.status(200).send();
+	res.status(200).send({
+		hello:'world'
+	});
 });
 
 module.exports = router;
--- tests/_api-tests.js
+++ tests/_api-tests.js
@@ -13,7 +13,27 @@ describe('API endpoint',function(){
 			var response;
 
 			before(function(done){
-				request(app).get('/scene')
+				request(app)
+					.post('/scene')
+					.send({
+						resource:{
+							type:'url',
+							location:'http://www.google.com'
+						}
+					})
+					.then(function(res){
+						return request(app)
+							.post('/scene')
+							.send({
+								resource:{
+									type:'url',
+									location:'http://www.github.com'
+								}
+							});
+					})
+					.then(function(res){
+						return request(app).get('/scene');
+					})
 					.then(function(res){
 						response = res;
 						done();
@@ -38,19 +58,40 @@ describe('API endpoint',function(){
 						}
 					}]);
 			});
+			
 			it('should respond with code 200 upon success',function(){
 				expect(response.statusCode).to.be(200);
 			});
 		});
 		describe('POST',function(){
-			it('should create a new scene',function(){
-				throw new NotImpementedError();
+			var response;
+
+			before(function(done){
+				request(app)
+					.post('/scene')
+					.send({
+						resource:{
+							type:'url',
+							location:'http://www.youtube.com'
+						}
+					})
+					.then(function(res){
+						response = res;
+						done();
+					})
+					.catch(function(err){
+						done(err);
+					});
 			});
+
+			// it('should create a new scene',function(){
+			// 	throw new NotImpementedError();
+			// });
 			it('should respond with code 201 upon success',function(){
-				throw new NotImpementedError();
+				expect(response.statusCode).to.be(201);
 			});
-			it('should return a Location header with a link to the newly-created resource',function(){
-				throw new NotImpementedError();
+			it('should return a `Location` header with a link to the newly-created resource',function(){
+				expect(response.headers.location).to.be.ok();
 			});
 		});
 	});
