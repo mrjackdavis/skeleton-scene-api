8c5dfc2a2d95199d58b52224fda3440ebbc877a2
Jack Davis <mr.jackdavis@gmail.com>
2015-04-19 14:12:47 +1000

    Setup secrets and basic dynamodb support

--- tests/_scene-store-tests.js
+++ tests/_scene-store-tests.js
@@ -1,18 +1,35 @@
 var NotImpementedError = require('../lib/NotImplementedError');
 var expect = require('expect.js');
 var SceneStore = require('../lib/stores/SceneStore');
+var appConfigGetter = require('../lib/AppConfig');
 
 describe('SceneStore',function(){
-	describe('Add',function(){
-		it('should create a new item',function(){
-			var store = new SceneStore({
+	var appConfig;
+	before(function(done){
+		appConfigGetter().then(function(config){
+			appConfig = config;
+			done();
+		}).catch(done);
+	});
 
+	describe('Add',function(){
+		it('should create a new item',function(done){
+			this.timeout(8000);
+			var store = new SceneStore({
+				accessKeyId:appConfig.TEST_AWS_CREDENTIALS.accessKeyId,
+				secretAccessKey:appConfig.TEST_AWS_CREDENTIALS.secretAccessKey
 			});
 
-			var scene = {'resource':{'type':'url','location':'http://la.com'},'processes':[]};
+			var scene = {
+				resource:{'type':'url','location':'http://la.com'},
+				processes:[],
+				dateCreated:(new Date()).getTime()
+			};
 
-			store.add(scene).then(function(scene){
+			store.Add(scene).then(function(scene){
 				expect(scene).to.be.ok();
+				expect(scene.sceneID).to.be.ok();
+
 				done();
 			}).catch(function(err){
 				done(err);
@@ -20,3 +37,4 @@ describe('SceneStore',function(){
 		});
 	});
 });
+
