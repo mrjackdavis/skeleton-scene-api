36ff813defc78fb75bc17091233bc226dcfc9e34
Jack Davis <mr.jackdavis@gmail.com>
2015-05-03 12:57:10 +1000

    Re-implement sceneStore.Get

--- tests/_scene-store-tests.js
+++ tests/_scene-store-tests.js
@@ -48,7 +48,6 @@ describe('SceneStore',function(){
 
 			store.Add(scene)
 				.then(function(scene){
-					console.log(scene);
 					expect(scene).to.be.ok();
 					expect(scene.sceneID).to.be.a('string');
 					expect(scene.dateCreated).to.be.a(Date);
